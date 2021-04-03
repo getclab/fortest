8fa43f4de009e2317adb012ac961e43a609d568e
andrii <75980132+getclab@users.noreply.github.com>
2021-04-03 15:50:54 +0300

    workwersion

--- main.py
+++ main.py
@@ -4,3 +4,10 @@ from bs4 import BeautifulSoup
 
 
 TOKEN = '1779694450:AAHrawLv8U5ccJxZnnBe_kFyV561EqXKBi0'
+bot = telebot.TeleBot(TOKEN, 'HTML')
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.send_message(message.chat.id, text='connect')
+
+bot.polling()
